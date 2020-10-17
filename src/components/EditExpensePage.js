884026f249c78b5858381948d75d8e68ea3832da
Akshay Antony <akshaya@valorem.com>
2020-10-17 13:13:07 +0530

    add query strings

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => (
     <div>
-      This is from my edit expense component
+      This is from my edit expense component with id of {props.match.params.id}
     </div>
   );
 
